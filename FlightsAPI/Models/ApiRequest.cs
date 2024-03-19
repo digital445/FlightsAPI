15932306636f3856cba97af98c6f0c6e90246b63
Aleksandr <digital445@gmail.com>
2024-03-19 11:41:29 +0500

    Add additional headers to the ApiRequest

--- FlightsAPI/Models/ApiRequest.cs
+++ FlightsAPI/Models/ApiRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace FlightsAPI.Models
+﻿using System.Net.Http.Headers;
+
+namespace FlightsAPI.Models
 {
 	public record ApiRequest(
 		HttpMethod Method,
+		Dictionary<string, string> AdditionalHeaders,
 		string Url,
 		object Data,
 		string AccessToken
