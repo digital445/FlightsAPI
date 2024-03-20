010751f7a4742f8c33d7934e957befecbc66aa20
Aleksandr <digital445@gmail.com>
2024-03-20 12:34:02 +0500

    Change Amadeus error and warning models

--- FlightsAPI/Models/Amadeus/ErrorsWarnings.cs
+++ FlightsAPI/Models/Amadeus/ErrorsWarnings.cs
@@ -1,7 +1,27 @@
 ﻿namespace FlightsAPI.Models.Amadeus
 {
-    public record Error(Issue[] Errors);
-    public record Warning(Issue[] Warnings);
-    public record Issue(long Code, string Title, string Detail, IssueSource Source);
-    public record IssueSource(string Pointer, string Parameter, string Example);
+	public record Error
+	{
+		public Issue[]? Errors { get; init; }
+	}
+
+	public record Warning
+	{
+		public Issue[]? Warnings { get; init; }
+	}
+
+	public record Issue
+	{
+		public long Code { get; init; } = -1;
+		public string? Title { get; init; }
+		public string? Detail { get; init; }
+		public IssueSource? Source { get; init; }
+	}
+
+	public record IssueSource
+	{
+		public string? Pointer { get; init; }
+		public string? Parameter { get; init; }
+		public string? Example { get; init; }
+	}
 }
