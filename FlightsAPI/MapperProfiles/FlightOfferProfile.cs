bec5e9bd12d5bd65b2321396306560ba9946ddd7
Aleksandr <digital445@gmail.com>
2024-03-22 12:44:35 +0500

    Add AutoMapper profiles

---
+++ FlightsAPI/MapperProfiles/FlightOfferProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FlightsAPI.Models;
+using FlightsAPI.Models.Amadeus;
+
+namespace FlightsAPI.MapperProfiles
+{
+	public class FlightOfferProfile : Profile
+	{
+		public FlightOfferProfile() 
+		{
+			CreateMap<AmadeusFlightOffer, FlightOffer>();
+		}
+	}
+}
+ 
---
+++ FlightsAPI/MapperProfiles/FlightQueryProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using FlightsAPI.Models.Amadeus;
+using FlightsAPI.Models;
+
+namespace FlightsAPI.MapperProfiles
+{
+	public class FlightQueryProfile : Profile
+	{
+		public FlightQueryProfile()
+		{
+			CreateMap<FlightQuery, AmadeusFlightQuery>()
+				.ForMember(dest => dest.OriginDestinations, opt => opt.MapFrom(src => new[] {
+					new OriginDestination { //flight
+						Id = "1",
+						OriginLocationCode = src.OriginLocationCode,
+						DestinationLocationCode = src.DestinationLocationCode,
+						DepartureDateTimeRange = new DateTimeRange {
+							Date = src.DepartureDate.Date, 
+							DateWindow = src.DepartureDate.DateWindow
+						}
+					},
+					new OriginDestination { //return flight
+						Id = "2",
+						OriginLocationCode = src.DestinationLocationCode,
+						DestinationLocationCode = src.OriginLocationCode,
+						DepartureDateTimeRange = new DateTimeRange {
+							Date = src.ReturnDate.Date,
+							DateWindow = src.ReturnDate.DateWindow
+						}
+					}
+				}))
+				.ForMember(dest => dest.Travelers, opt => opt.MapFrom(src => 
+					Enumerable.Range(1, src.PassengerAmount)
+						.Select(i => new ExtendedTravelInfo { Id = i.ToString() }) 
+						.ToArray()
+				));
+		}
+	}
+}
