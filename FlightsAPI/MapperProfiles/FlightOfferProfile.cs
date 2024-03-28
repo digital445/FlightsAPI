9839ab6b91add55f5b6e5e883b214aa1123faf95
Aleksandr <digital445@gmail.com>
2024-03-28 10:14:39 +0500

    Add a mapping profile for flight search results

--- FlightsAPI/MapperProfiles/FlightOfferProfile.cs
+++ FlightsAPI/MapperProfiles/FlightOfferProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using FlightsAPI.Models;
 using FlightsAPI.Models.Amadeus;
+using System.Xml;
+using static FlightsAPI.Enumerations;
 
 namespace FlightsAPI.MapperProfiles
 {
@@ -8,7 +10,22 @@ namespace FlightsAPI.MapperProfiles
 	{
 		public FlightOfferProfile() 
 		{
-			CreateMap<AmadeusFlightOffer, FlightOffer>();
+			CreateMap<AmadeusFlightOffer, FlightOffer>()
+				.ForMember(dest => dest.FlightProvider, opt => opt.MapFrom(src => FlightProvider.Amadeus));
+			CreateMap<AmItinerary, Itinerary>()
+				.ForMember(dest => dest.Duration, opt => opt.MapFrom(src => ConvertToTimeSpan(src.Duration)));
+			CreateMap<AmSegment, Segment>()
+				.ForMember(dest => dest.Duration, opt => opt.MapFrom(src => ConvertToTimeSpan(src.Duration)));
+			CreateMap<AmAirport, Airport>();
+			CreateMap<AmAircraft, Aircraft>();
+			CreateMap<AmPrice, Price>();
+			CreateMap<AmTravelerPricing, TravelerPricing>();
+			CreateMap<AmFareDetailsBySegment, FareDetailsBySegment>();
+		}
+
+		private static TimeSpan? ConvertToTimeSpan(string? duration)
+		{
+			return duration != null ? XmlConvert.ToTimeSpan(duration) : null;
 		}
 	}
 }
