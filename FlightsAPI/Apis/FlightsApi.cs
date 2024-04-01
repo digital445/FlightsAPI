e8da50a6b88dbf0804bac2d10af2db0fb5215344
Aleksandr <digital445@gmail.com>
2024-04-01 13:49:26 +0500

    Add a small query Validation to FlightsApi

--- FlightsAPI/Apis/FlightsApi.cs
+++ FlightsAPI/Apis/FlightsApi.cs
@@ -18,14 +18,27 @@ namespace FlightsAPI.Apis
 			FlightQuery query, 
 			IFlightService flightService)
 		{
+			ValidateQuery(query);
 
 			var flightOffers = await flightService.GetFlightOffers(query);
 			if (flightOffers.Any())
-			{ 
+			{
 				return TypedResults.Ok(flightOffers);
 			}
 			return TypedResults.NoContent();
 		}
+
+		private static void ValidateQuery(FlightQuery query)
+		{
+			ArgumentNullException.ThrowIfNull(query);
+			ArgumentNullException.ThrowIfNull(query.DepartureDate);
+			if (query.ReturnDate != null &&
+				query.ReturnDate.Date < query.DepartureDate.Date)
+			{
+				throw new ArgumentException("The return flight should be after the outbound flight.");
+			}
+		}
+
 		private static async Task<Results<Ok<IEnumerable<FlightOffer>>, IResult>> BookFlights(
 			BookingOrder query, 
 			IFlightService flightService)
